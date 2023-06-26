81bd81489c182d3f7e7f4487d2682ea7a658c526
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-26 11:46:53 +0100

    010-recursos

--- Francisco_26-06-2023/mygame/main.lua
+++ Francisco_26-06-2023/mygame/main.lua
@@ -1,13 +1,13 @@
 function love.load()
     --Escreve o cÃ³digo para carregar a imagem na linha baixo
-
+    imagem = love.graphics.newImage("nave2.png")
 end
 
 
 function love.draw()
-    love.graphics.print("Ola mundo!", 100, 100);
+    love.graphics.print("Ola seu gay!", 50, 50);
     --Escreve o cÃ³digo para desenhar a imagem na linha baixo
-
+    love.graphics.draw(imagem, 100, 100, 0, 1, 1)
 end
 
 function love.update(dt)
