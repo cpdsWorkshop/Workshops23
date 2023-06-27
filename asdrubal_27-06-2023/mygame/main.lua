cb8906166cf165b2c659ac870ac2543497f6caee
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-27 15:50:13 +0100

    013-jogador

--- asdrubal_27-06-2023/mygame/main.lua
+++ asdrubal_27-06-2023/mygame/main.lua
@@ -1,12 +1,14 @@
 function love.load()
     --Escreve o cÃ³digo para carregar a imagem na linha baixo
     imagem = love.graphics.newImage("nave1.png")
+    posicaoX = 0
+    posicaoY = 0
 end
 
 
 function love.draw()
     --Escreve o cÃ³digo para desenhar a imagem na linha baixo
-    love.graphics.draw(imagem, 100, 100, 0, 1, 1)
+    love.graphics.draw(imagem, posicaoX, posicaoY, 0, 1, 1)
 end
 
 function love.update(dt)
