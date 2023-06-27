58f5d92b9becd31ef165c0aa0bbdd9b1fca77123
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-27 14:36:30 +0100

    020-missel-load

--- jorge_27-06-2023/mygame/main.lua
+++ jorge_27-06-2023/mygame/main.lua
@@ -4,15 +4,31 @@ function love.load()
     posicaoX = 0
   posicaoY = 0
   velocidade = 15
+
+  missilImagem = love.graphics.newImage("missil1.png")
+misseis = {}
+
 end
 
 
 function love.draw()
     --Escreve o cÃ³digo para desenhar a imagem na linha baixo
     love.graphics.draw(imagem, posicaoX, posicaoY, 0, 1, 1)
+
+    for index, missil in ipairs(misseis) do
+        love.graphics.draw(missil.imagem, missil.posicaoX,
+    missil.posicaoY)
+    end
 end
 
 function love.update(dt)
+
+    if love.keyboard.isDown("space") then
+        missil = {posicaoX = posicaoX, posicaoY = posicaoY,
+    velocidade = 300, imagem = missilImagem}
+        table.insert(misseis, missil)
+    end
+    
     if posicaoY > 0 then
         if love.keyboard.isDown("up") then
             posicaoY = posicaoY - velocidade
