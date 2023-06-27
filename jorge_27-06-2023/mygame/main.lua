4844835e413ccfefd0dffcaa1e6c59e648dd02ea
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-27 14:32:44 +0100

    018-speed

--- jorge_27-06-2023/mygame/main.lua
+++ jorge_27-06-2023/mygame/main.lua
@@ -3,6 +3,7 @@ function love.load()
     imagem = love.graphics.newImage("nave1.png")
     posicaoX = 0
   posicaoY = 0
+  velocidade = 15
 end
 
 
@@ -14,22 +15,22 @@ end
 function love.update(dt)
     if posicaoX < (love.graphics.getWidth() - imagem:getWidth()) then
         if love.keyboard.isDown("right") then
-            posicaoX = posicaoX + 15
+            posicaoX = posicaoX +  velocidade
         end
     end
     if posicaoX > 0 then
         if love.keyboard.isDown("left") then
-            posicaoX = posicaoX - 15
+            posicaoX = posicaoX - velocidade
         end
     end
     if posicaoY <(love.graphics.getHeight() - imagem:getHeight()) then
         if love.keyboard.isDown("down") then
-            posicaoY = posicaoY + 15
+            posicaoY = posicaoY + velocidade
         end
     end
     if posicaoY > 0 then
         if love.keyboard.isDown("up") then
-            posicaoY = posicaoY - 15
+            posicaoY = posicaoY - velocidade
         end
     end
 end
