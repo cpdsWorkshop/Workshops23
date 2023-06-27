b9526826a2fcb0dfc91b1b02716aac2bd6d4a7d0
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-27 15:52:30 +0100

    016-Movimentacao

--- asdrubal_27-06-2023/mygame/main.lua
+++ asdrubal_27-06-2023/mygame/main.lua
@@ -12,5 +12,16 @@ function love.draw()
 end
 
 function love.update(dt)
-
+    if love.keyboard.isDown("right") then
+        posicaoX = posicaoX + 1
+    end
+    if love.keyboard.isDown("left") then
+        posicaoX = posicaoX - 1
+    end
+    if love.keyboard.isDown("up") then
+        posicaoY = posicaoY - 1
+    end
+    if love.keyboard.isDown("down") then
+        posicaoY = posicaoY + 1
+    end
 end
