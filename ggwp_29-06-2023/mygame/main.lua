c2e5d63bff8bf2ce4e18cd38b6bb46e5a11ffc5c
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-29 14:22:38 +0100

    007-code

--- ggwp_29-06-2023/mygame/main.lua
+++ ggwp_29-06-2023/mygame/main.lua
@@ -1,15 +1,4 @@
-function love.load()
-    --Escreve o cÃ³digo para carregar a imagem na linha baixo
-
-end
-
-
 function love.draw()
-    love.graphics.print("Ola mundo!", 200, 200);
-    --Escreve o cÃ³digo para desenhar a imagem na linha baixo
-
+        love.graphics.print("Ola mundo!", 100, 100);
 end
 
-function love.update(dt)
-
-end
