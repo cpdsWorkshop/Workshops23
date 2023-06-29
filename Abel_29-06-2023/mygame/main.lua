8b6dc586516a2a1b3a9731cb890a68778ed755e9
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-06-29 16:52:59 +0100

    010-recursos

--- Abel_29-06-2023/mygame/main.lua
+++ Abel_29-06-2023/mygame/main.lua
@@ -1,13 +1,14 @@
 function love.load()
     --Escreve o cÃ³digo para carregar a imagem na linha baixo
+    imagem = love.graphics.newImage("missil1.png")
 
 end
 
 
 function love.draw()
-    love.graphics.print("Ola mundo!", 200, 200);
+    love.graphics.print("Abel", 200, 200);
     --Escreve o cÃ³digo para desenhar a imagem na linha baixo
-
+    love.graphics.draw(imagem, 100, 100, 0, 1, 1)
 end
 
 function love.update(dt)
