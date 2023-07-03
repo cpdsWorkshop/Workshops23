1c9eba6544bd78b050ddac1dc74bf6cfcb7e10bf
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-07-03 10:58:32 +0100

    013-jogador

--- Gorete_03-07-2023/mygame/main.lua
+++ Gorete_03-07-2023/mygame/main.lua
@@ -1,10 +1,12 @@
 function love.load()
-    imagem = love.graphics.newImage("nave3.png");
+    imagem = love.graphics.newImage("nave1.png");
+    posicaoX = 300;
+    posicaoY = 300;
 end
 
 
 function love.draw()
-    love.graphics.draw(imagem, 100, 100, 0, 1, 1);
+    love.graphics.draw(imagem, posicaoX, posicaoY, 0, 1, 1);
 end
 
 function love.update(dt)
