922b9e02afae4ce9b49f1361c9047f803569e817
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-07-03 10:56:51 +0100

    010-recursos

--- Gorete_03-07-2023/mygame/main.lua
+++ Gorete_03-07-2023/mygame/main.lua
@@ -1,13 +1,10 @@
 function love.load()
-    --Escreve o cÃ³digo para carregar a imagem na linha baixo
-
+    imagem = love.graphics.newImage("nave3.png");
 end
 
 
 function love.draw()
-    love.graphics.print("Ola mundo!", 200, 200);
-    --Escreve o cÃ³digo para desenhar a imagem na linha baixo
-
+    love.graphics.draw(imagem, 100, 100, 0, 1, 1);
 end
 
 function love.update(dt)
