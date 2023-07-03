f405e988597f26061a0ccdea01d3401300803e1c
cpdsWorkshop <135114200+cpdsWorkshop@users.noreply.github.com>
2023-07-03 11:05:20 +0100

    013-jogador

--- Gorete_03-07-2023/mygame/main.lua
+++ Gorete_03-07-2023/mygame/main.lua
@@ -10,5 +10,17 @@ function love.draw()
 end
 
 function love.update(dt)
-
+    if love.keyboard.isDown("right") then
+        posicaoX = posicaoX + 1
+    end
+    if love.keyboard.isDown("left") then
+        posicaoX = posicaoX - 1
+    end
+    if love.keyboard.isDown("down") then
+        posicaoY = posicaoY + 1
+    end
+    if love.keyboard.isDown("up") then
+        posicaoY = posicaoY - 1
+    end
+    
 end
